dfb8ca3b1a836eec18ed6130d40baef899676ff1
danielDMC <danieldemariaycampos@MacBook-Pro-de-Daniel.local>
2017-05-29 23:12:09 -0500

    Borrador créditos

---
+++ creditos.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include('head.php'); ?>
+	<body>
+		<?php include('script.php'); ?>
+		<div id="wrapper">
+			<!-- Navigation -->
+			<h1 align="center">UVAQ</h1>
+			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" align="center">
+				<?php include("navbarHeader.php"); include("sidebar.php"); ?>
+				<input type="button" name="" value="Contacto">
+				<input type="button" name="" value="Menú Principal">
+				<input type="button" name="" value="Quejas y Sugerencias">
+			</nav><br><br>
+			<div id="page-wrapper">
+				<div class="container-fluid">
+					<section class="container" align="center">
+						<h2><b>Facultad De Ingenierías</b></h2>
+						<div class="container-page">
+
+
+							<table align="center">
+								<th><h3>Proyecto Desarrollado Por:</h3></th>
+								<tr><td align="center"><li>Hector Daiel López Mendoza</li></td></tr>
+								<tr><td><li>Pablo Efraín Cipres Valdez</li></td></tr>
+								<tr><td><li>Abel Velazques Portillo</li></td></tr>
+								<tr><td><li>Sebastian Constancio Arciga Díaz</li></td></tr>
+								<tr><td><li>Edgar Jaime Mendoza Coronel</li></td></tr>
+								<tr><td><li>Daniel de Maria y Campos Llorente</li></td></tr>
+								<tr>
+									<th><h3>Con Asesoría y Supervisión de:</h3></th>
+
+								</tr>
+								<tr>
+									<td><li>Ing. Jorge Regalado Meza</li></td>
+								</tr>
+
+
+
+
+
+							</table>
+
+
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
